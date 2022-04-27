2c61c8f20489399e09c41e8750198499ec5fffc9
Lonxyboi <drewthemachine22@gmail.com>
2022-04-27 18:51:41 -0400

    added questions to script

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -5,7 +5,72 @@ const buttonEl = document.querySelector('#btn');
 let timer
 var timeLeft = 60;
 
+var question1 = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: {
+            a: "strings",
+            b: "boonleans",
+            c: "alerts",
+            d: "numbers"
+        }
+    }
+];
+
+var question2 = [
+    {
+        question: "The condition of an if/else if statement is enclosed in:",
+        answers: {
+            a: "quotes",
+            b: "parenthases",
+            c: "curly brackets",
+            d: "square brackets"
+        }
+    }
+];
+
+var question3 = [
+    {
+        question: "Arrays in JavaScript can be used to store______.",
+        answers: {
+            a: "numbers and strings",
+            b: "boonleans",
+            c: "other arrays",
+            d: "all of the above"
+        }
+    }
+];
+
+var question4 = [
+    {
+        question: "String values must be enclosed in ____ when assigned to variables.",
+        answers: {
+            a: "curly brackets",
+            b: "commas",
+            c: "quotes",
+            d: "parenthesis"
+        }
+    }
+];
+
+var question5 = [
+    {
+        question: "A very useful tool in web development and for debugging for printing content to the debugger in:",
+        answers: {
+            a: "JavaScript",
+            b: "terminal/ bash",
+            c: "for loops",
+            d: "console.log"
+        }
+    }
+];
+
+
+
+
 buttonEl.addEventListener('click', startQuiz);
+
+
 
 function startQuiz(){
     displaySection(1);
